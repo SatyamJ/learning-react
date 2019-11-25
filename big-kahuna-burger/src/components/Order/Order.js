7fe23eba38a64f03ec37d98042640e7a25cee83a
Satyam Jaiswal <snjaiswa@asu.edu>
2019-11-24 16:27:58 -0800

    orders

--- big-kahuna-burger/src/components/Order/Order.js
+++ big-kahuna-burger/src/components/Order/Order.js
@@ -2,10 +2,32 @@ import React from 'react'
 import classes from './Order.css';
 
 const order = (props) => {
+    const ingredients = [];
+    for (const key in props.ingredients) {
+        ingredients.push(
+            {
+                ingredientName: key,
+                amount: props.ingredients[key]
+            }
+        )
+    }
+    const ingredientsOutput = ingredients.map(ig => {
+        return (
+            <span style={{
+                textTransform: 'Capitalize',
+                display: 'inline-block',
+                margin: '0 8px',
+                padding: '5px',
+                border: '1px solid #ccc'
+            }}>
+                {ig.ingredientName} ({ig.amount})
+            </span>
+        )
+    });
     return (
         <div className={classes.Order}>
-            <p>Ingredients: Salad(1), Cheese(2)</p>
-            <p>Price: <strong>USD 5</strong></p>
+            <p>Ingredients: {ingredientsOutput}</p>
+            <p>Price: <strong>USD {props.price}</strong></p>
         </div>
     )
 };
--- big-kahuna-burger/src/containers/Orders/Orders.js
+++ big-kahuna-burger/src/containers/Orders/Orders.js
@@ -3,22 +3,47 @@ import Order from '../../components/Order/Order';
 import axios from '../../axios-orders';
 
 class Orders extends Component {
-    
+    state = {
+        orders: [],
+        loading: false
+    };
+
     componentDidMount() {
         axios.get('/orders.json')
             .then(response => {
                 console.log(response.data);
+                const fetchOrders = [];
+                for (const key in response.data) {
+                    fetchOrders.push(
+                        {
+                            ...response.data[key],
+                            id: key
+                        }
+                    )
+                }
+                this.setState({
+                    orders: fetchOrders,
+                    loading: false
+                })
             })
             .catch(error => {
                 console.log(error);
+                this.setState({
+                    loading: false
+                })
             })
     }
 
     render() {
+        const orders = this.state.orders.map(order => {
+            return <Order price={order.price}
+                          ingredients={order.ingredients}
+                          key={order.id}
+            />
+        });
         return (
             <div>
-                <Order/>
-                <Order/>
+                {orders}
             </div>
         )
     }
